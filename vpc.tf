4164ddadba0c4c48e387322272564de121b86ae2
SwapnilManohar <71309232+SwapnilManohar@users.noreply.github.com>
2022-01-31 11:49:38 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -8,7 +8,6 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["eu-west-1a"]
-  private_subnets = ["10.0.1.0/24"]
   public_subnets  = ["10.0.101.0/24"]
 
   enable_nat_gateway = true
