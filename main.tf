21aed40aa73e6c280f57d69ee756d50677cbaf0d
SwapnilManohar <71309232+SwapnilManohar@users.noreply.github.com>
2022-01-31 15:45:05 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_subnet" "dev-public-1" {
   vpc_id                  = aws_vpc.dev.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "ap-south-1a"
+  availability_zone       = "us-east-1a"
 
   tags = {
     Name = "dev-public-1"
@@ -30,7 +30,7 @@ resource "aws_subnet" "dev-public-2" {
   vpc_id                  = aws_vpc.dev.id
   cidr_block              = "10.0.2.0/24"
   map_public_ip_on_launch = "true"
-  availability_zone       = "ap-south-1b"
+  availability_zone       = "us-east-1a"
 
   tags = {
     Name = "dev-public-2"
